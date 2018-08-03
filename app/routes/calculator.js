2e634b55b70113a250ceb4164bf81c7ffd7cb668
Philippe Schommers <philippe@schommers.be>
2018-08-03 19:08:51 +0200

    Separate routes and add option to fetch price in other currencies

--- app/routes/calculator.js
+++ app/routes/calculator.js
@@ -11,28 +11,40 @@ const influx = new Influx.InfluxDB(config.get('influxdb'))
 const router = express.Router()
 
 // Routes
-router.get('/', async (req, res) => {
-  const coin = await rp({
-    uri: 'https://api.coinmarketcap.com/v2/ticker/1923/',
-    json: true
-  })
+router.get('/active-nodes', async (req, res) => {
+  const query = await influx.query(`
+    SELECT mean("total_active_nodes")
+    FROM "autogen"."core"
+    WHERE time >= now() - 30d
+    GROUP BY time(6h) fill(previous)
+  `)
 
-  const graphData = (await influx.query(`
-      SELECT mean("total_active_nodes")
-      FROM "autogen"."core"
-      WHERE time >= now() - 30d
-      GROUP BY time(6h) fill(previous)
-    `))
+  const graphData = query
     .map(value => ({
       name: value.time.toISOString(),
       nodes: Math.round(value.mean)
     }))
     .filter(value => !!value.nodes)
 
-  res.json({
-    graphData,
-    tntPrice: coin.data.quotes.USD.price
+  res.json(graphData)
+})
+
+router.get('/price/:currency?', async (req, res) => {
+  const currency = (req.params.currency || 'USD').toUpperCase()
+  const coin = await rp({
+    uri: `https://api.coinmarketcap.com/v2/ticker/1923/?convert=${currency}`,
+    json: true
   })
+
+  Object.keys(coin.data.quotes).forEach(currency => {
+    coin.data.quotes[currency] = coin.data.quotes[currency].price
+  })
+
+  res.json(coin.data.quotes)
+})
+
+router.get('/currencies', (req, res) => {
+  res.json(config.get('coinmarketcap.currencies'))
 })
 
 // Export
--- config/default.js
+++ config/default.js
@@ -3,5 +3,44 @@ module.exports = {
     database: 'tierion',
     host: 'localhost',
     port: 8086
+  },
+  coinmarketcap: {
+    currencies: {
+      fiat: [
+        'AUD',
+        'BRL',
+        'CAD',
+        'CHF',
+        'CLP',
+        'CNY',
+        'CZK',
+        'DKK',
+        'EUR',
+        'GBP',
+        'HKD',
+        'HUF',
+        'IDR',
+        'ILS',
+        'INR',
+        'JPY',
+        'KRW',
+        'MXN',
+        'MYR',
+        'NOK',
+        'NZD',
+        'PHP',
+        'PKR',
+        'PLN',
+        'RUB',
+        'SEK',
+        'SGD',
+        'THB',
+        'TRY',
+        'TWD',
+        'USD',
+        'ZAR'
+      ],
+      crypto: ['BTC', 'ETH', 'XRP', 'LTC', 'BCH']
+    }
   }
 }
